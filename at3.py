66e36869bca5c23ddeaa0b829ff51dc546446a93
noguixz7 <noguilhermek@gmail.com>
2026-04-22 15:26:56 -0300

    coisas simples 3.3

--- at3.py
+++ at3.py
@@ -6,3 +6,13 @@ while contador < 67:
     print(contador)
     contador += 1
     time.sleep(0.01)
+
+#atividade
+n1 = int(input("coloque um numero: "))
+if n1 < 1:
+    while n1 < 101:
+        print(n1)
+        n1 += 1
+        time.sleep(0.01)
+else:
+    print("Variavel maior que 0")
